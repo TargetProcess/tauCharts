f6c84dbbd901ab44f38176d42254a1a05b2bc5a8
Michael Dubakov <mdubakov@gmail.com>
2014-03-22 17:35:42 +0300

    charts moved into separate files

---
+++ src/charts/scatterplot.js
@@ -0,0 +1,32 @@
+(function() {
+
+	/**@class */
+    /**@extends BasicChart */
+    var ScatterPlotChart = tau.charts.Base.extend({
+
+        _renderData: function (container, data) {
+            this._mapper.binder('size').domain(d3.extent(data, this._mapper.raw('size'))); // TODO: should be done automatically using chart metadata
+
+            var mapper = this._mapper;
+
+            var update = function () {
+                return this
+                    .attr('class', mapper.map('dot i-role-datum %color%'))
+                    .attr('r', mapper.map('size'))
+                    .attr('cx', mapper.map('x'))
+                    .attr('cy', mapper.map('y'));
+            };
+
+            var elements = container.selectAll('.dot').data(data);
+
+            elements.call(update);
+            elements.enter().append('circle').call(update);
+            elements.exit().remove();
+        }
+    });
+
+    tau.charts.add('Scatterplot', function (data) {
+        return new ScatterPlotChart(data);
+    });
+
+})();
--- src/tau.charts.js
+++ src/tau.charts.js
@@ -268,99 +268,7 @@
         }
     });
 
-    /**@class */
-    /**@extends BasicChart */
-    var ScatterPlotChart = BasicChart.extend({
-
-        _renderData: function (container, data) {
-            this._mapper.binder('size').domain(d3.extent(data, this._mapper.raw('size'))); // TODO: should be done automatically using chart metadata
-
-            var mapper = this._mapper;
-
-            var update = function () {
-                return this
-                    .attr('class', mapper.map('dot i-role-datum %color%'))
-                    .attr('r', mapper.map('size'))
-                    .attr('cx', mapper.map('x'))
-                    .attr('cy', mapper.map('y'));
-            };
-
-            var elements = container.selectAll('.dot').data(data);
-
-            elements.call(update);
-            elements.enter().append('circle').call(update);
-            elements.exit().remove();
-        }
-    });
-
-
-    /**@class */
-    /**@extends BasicChart */
-    var LineChart = BasicChart.extend({
-        map: function (config) {
-            this._super(config);
-            this._mapper.alias('color', 'key');
-
-            return this;
-        },
-        _renderData: function (container, data) {
-            var mapper = this._mapper;
-
-            // prepare data to build several lines
-            // TODO: provide several data transformers to support more formats
-            // sometime we will have data already nested, for example.
-            var categories = d3.nest()
-                .key(mapper.raw('color'))
-                .entries(data);
-
-            var updateLines = function () {
-                this.attr('class', mapper.map('line %color%'));
-
-                var paths = this.selectAll('path').data(function (d) {
-                    return [d.values];
-                });
-
-                // TODO: extract update pattern to some place
-                paths.call(updatePaths);
-                paths.enter().append('path').call(updatePaths);
-                paths.exit().remove();
-
-                var dots = this.selectAll('.dot').data(function (d) {
-                    return d.values;
-                });
-
-                dots.call(updateDots);
-                dots.enter().append('circle').attr('class', 'dot i-role-datum').call(updateDots);
-                dots.exit().remove();
-            };
-
-            //TODO: allow to set interpolation outside
-            var line = d3.svg.line()
-                .interpolate('cardinal')
-                .x(mapper.map('x'))
-                .y(mapper.map('y'));
-
-            var updatePaths = function () {
-                this.attr('d', line);
-            };
-
-            var updateDots = function () {
-                // draw circles (to enable mouse interactions)
-                return this
-                    .attr('cx', mapper.map('x'))
-                    .attr('cy', mapper.map('y'))
-                    .attr('r', function () {
-                        return 3;
-                    });
-            };
-
-            var lines = container.selectAll('.line').data(categories);
-            lines.call(updateLines);
-            lines.enter().append('g').call(updateLines);
-            lines.exit().remove();
-        }
-    });
-
+    
     /** @class ChartTools */
     var ChartTools = Class.extend({
         /**
@@ -409,13 +317,16 @@
     tau.charts = {
         /**
          * @param data
-         * @returns {ScatterPlotChart}
+         * @returns {BasicChart}
          */
-        Scatterplot: function (data) {
-            return new ScatterPlotChart(data);
-        },
-        Line: function (data) {
-            return new LineChart(data);
+        Base: BasicChart,
+
+        /**
+        * Register new chart
+        */
+        add: function(name, fn) {
+            tau.charts[name] = fn;    
         }
     };
+    
 })();
