84fbca806039a2735f8942dea267574198c279b1
unutranyholas <unutranyholas@gmail.com>
2014-03-28 13:35:58 +0300

    Add simple header plugin

--- build/tauCharts.js
+++ build/tauCharts.js
@@ -1,4 +1,4 @@
-/*! tauCharts - v0.0.1 - 2014-03-27
+/*! tauCharts - v0.0.1 - 2014-03-28
 * https://github.com/TargetProcess/tauCharts
 * Copyright (c) 2014 Taucraft Limited; Licensed MIT */
 (function () {
@@ -988,6 +988,39 @@
     };
 
     tau.plugins.add('datatable', DataTable);
+})();
+(function () {
+    /** @class DataTable
+     * @extends Plugin */
+    var Header = {
+        /**
+         * @param {RenderContext} context
+         * @param {ChartTools} tools
+         */
+        init: function (){
+            this._header = arguments[0];
+            this._description = arguments[1];
+        },
+        render: function (context, tools) {
+
+            var header = this._header;
+            var description = this._description;
+
+            var container = tools.html.above
+                .append('header')
+                .attr('class', 'title');
+
+            container
+                .append('h1')
+                .html(header);
+
+            container    
+                .append('p')
+                .html(description);
+        }
+    };
+
+    tau.plugins.add('header', Header);
 })();
 (function () {
     /** @class Tooltip
