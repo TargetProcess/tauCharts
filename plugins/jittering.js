5f44624596eafdfe99751150ca3f06f592de38e5
unutranyholas <unutranyholas@gmail.com>
2014-04-18 18:53:40 +0300

    Added checkbox

--- plugins/jittering.js
+++ plugins/jittering.js
@@ -1,4 +1,5 @@
 (function () {
+
     /** @class Legend
      * @extends Plugin */
     var Jittering = {
@@ -6,8 +7,23 @@
          * @param {RenderContext} context
          * @param {ChartTools} tools
          */
+
         render: function (context, tools) {
 
+            var container = tools.html
+                .right
+                .append('div')
+                .attr('class', 'jittering')
+                .append('label');
+
+            var selector = container
+                .insert('input', ':first-child')
+                .attr('type', 'checkbox')
+                .attr('id', 'applyJittering');
+
+            container
+                .append('span')
+                .text('Jittering');
         }
     };
 
