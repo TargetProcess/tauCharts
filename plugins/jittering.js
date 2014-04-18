72ea8bda2083d3f94234010e1c73a15d00dffaa5
unutranyholas <unutranyholas@gmail.com>
2014-04-18 13:41:05 +0300

    Added jittering plugin

---
+++ plugins/jittering.js
@@ -0,0 +1,14 @@
+    /** @class Legend
+     * @extends Plugin */
+    var Jittering = {
+        /**
+         * @param {RenderContext} context
+         * @param {ChartTools} tools
+         */
+        render: function (context, tools) {
+
+        }
+    };
+
+    tau.plugins.add('jittering', Jittering);
+})();
