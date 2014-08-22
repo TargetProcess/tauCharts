64e1aea9e7283fa17b42f21fa242bc546064fc5e
Hanna Shniukava <anna.dev@targetprocess.com>
2014-08-22 16:00:06 +0300

    Fixed error in bower.json

--- build/tauCharts.js
+++ build/tauCharts.js
@@ -1,4 +1,4 @@
-/*! tauCharts - v0.0.1 - 2014-05-30
+/*! tauCharts - v0.0.1 - 2014-08-22
 * https://github.com/TargetProcess/tauCharts
 * Copyright (c) 2014 Taucraft Limited; Licensed MIT */
 (function () {
