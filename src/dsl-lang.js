2b98ec5c85da040df9621f0f32c1f832c59b114a
Vladimir Petriko <vlad@targetprocess.com>
2014-10-07 18:55:06 +0300

    Short GoG DSL language sample

---
+++ src/dsl-lang.js
@@ -0,0 +1,48 @@
+var COORDS = {
+    RECT: function(dim, tail) {
+        var subUnits = _.isArray(tail) ? tail : [tail];
+        var isFacet = (subUnits[0].type === 'COORDS.RECT');
+        return {
+            type: 'COORDS.RECT',
+            func: (isFacet ? 'CROSS' : ''),
+            showGridLines: (isFacet ? '' : 'xy'),
+            padding: {L: 64, B: 32, R: 8, T: 8},
+            axes: dim,
+            unit: subUnits
+        };
+    }
+};
+
+var DIM = function(x, y) {
+    return [
+        x ? {scaleDim: x} : null,
+        y ? {scaleDim: y} : null
+    ];
+};
+
+var ELEMENT = {
+    POINT: function (x, y, color, size) {
+        return {
+            type: 'ELEMENT.POINT',
+            x: x,
+            y: y,
+            color: color,
+            size: size
+        };
+    }
+};
+
+export {COORDS};
+export {DIM};
+export {ELEMENT};
+
+//{
+//    dimensions: {
+//        project: { scaleType: 'ordinal' },
+//        team: { scaleType: 'ordinal' },
+//        cycleTime: { scaleType: 'linear' },
+//        effort: { scaleType: 'linear' }
+//    },
+//    data: [],
+//    unit: COORDS.RECT(DIM('project', 'team'), COORDS.RECT(DIM('cycleTime', 'effort'), ELEMENT.POINT('cycleTime', 'effort')))
+//}
