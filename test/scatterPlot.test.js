7810d7a9606f89fb3a90e44e30019ea928b0e8bd
konstantin <krivlenia@targetprocess.com>
2014-10-09 20:59:17 +0300

    add test on scatter plot

---
+++ test/scatterPlot.test.js
@@ -0,0 +1,16 @@
+describe('scatter plot chart', function () {
+    var testData = [
+        {x: 1, y: 1, color: 'red', size: 6},
+        {x: 0.5, y: 0.5, color: 'green', size: 6},
+        {x: 2, y: 2, color: 'green', size: 8}
+    ];
+    it('should convert to common config', function () {
+        var scatter = new tauChart.Scatterplot({
+            data:testData,
+            x:'x',
+            y:'y',
+            color:'color',
+            size:'size'
+        });
+    })
+});
