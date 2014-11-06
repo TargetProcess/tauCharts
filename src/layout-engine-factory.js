058c8f0e5aea79ed2bd9a8a502532f9da7499a85
Vladimir Petriko <vlad@targetprocess.com>
2014-11-06 21:18:01 +0300

    BUG#91727. Incorrect axes extraction is fixed.

--- src/layout-engine-factory.js
+++ src/layout-engine-factory.js
@@ -2,6 +2,41 @@ import {utils} from './utils/utils';
 import {utilsDraw} from './utils/utils-draw';
 import {TMatrix} from './matrix';
 
+
+var specUnitSummary = (spec, boxOpt) => {
+    var box = boxOpt ? boxOpt : {depth: -1, paddings: []};
+    var p = (spec.guide || {}).padding || {l: 0, b: 0, r: 0, t: 0};
+    box.depth += 1;
+    box.paddings.unshift({l: p.l, b: p.b, r: p.r, t: p.t});
+
+    if (spec.unit && spec.unit.length) {
+        specUnitSummary(spec.unit[0], box);
+    }
+
+    return box;
+};
+
+var fnApplyDefaults = (rootNode) => {
+
+    var fnTraverseLayout = (rawNode) => {
+
+        var node = utilsDraw.applyNodeDefaults(rawNode);
+
+        if (!node.$matrix) {
+            return node;
+        }
+
+        node.$matrix.iterate((iRow, iCol, subNodes) => {
+            subNodes.forEach(fnTraverseLayout);
+        });
+
+        return node;
+    };
+
+    return fnTraverseLayout(rootNode);
+};
+
+
 var fnDefaultLayoutEngine = (rootNode, domainMixin) => {
 
     var fnTraverseLayout = (rawNode) => {
@@ -64,6 +99,133 @@ var fnDefaultLayoutEngine = (rootNode, domainMixin) => {
 var LayoutEngineTypeMap = {
 
     'DEFAULT': fnDefaultLayoutEngine,
+
+    'EXTRACT': (rootNode, domainMixin) => {
+
+        var traverse = ((rootNodeMatrix, totalBox, depth, rule) => {
+
+            var matrix = rootNodeMatrix;
+
+            var rows = matrix.sizeR();
+            var cols = matrix.sizeC();
+
+            matrix.iterate((r, c, subNodes) => {
+
+                var newNodes = subNodes
+                    .map((unit) => {
+                        return rule(unit, {
+                            firstRow: (r === 0),
+                            firstCol: (c === 0),
+                            lastRow: (r === (rows - 1)),
+                            lastCol: (c === (cols - 1)),
+                            depth: depth
+                        });
+                    })
+                    .filter((unit) => unit !== null);
+
+                matrix.setRC(r, c, newNodes);
+
+                var newTotalBox = utils.clone(totalBox);
+
+                matrix
+                    .getRC(r, c)
+                    .filter((unit) => unit.$matrix)
+                    .forEach((unit) => {
+                        unit.$matrix = new TMatrix(unit.$matrix.cube);
+                        traverse(unit.$matrix, newTotalBox, depth - 1, rule);
+                    });
+            });
+        });
+
+        var normalizedNode = fnApplyDefaults(rootNode);
+
+        var coordNode = utils.clone(normalizedNode);
+        var elemsNode = utils.clone(normalizedNode);
+
+        var coordMatrix = new TMatrix([[[coordNode]]]);
+        var elemsMatrix = new TMatrix([[[elemsNode]]]);
+
+        var box = specUnitSummary(coordNode);
+
+        var globPadd = box.paddings.reduce(
+            (memo, item) => {
+                memo.l += item.l;
+                memo.b += item.b;
+                return memo;
+            },
+            {l: 0, b: 0});
+
+        var temp = utils.clone(globPadd);
+        var axesPadd = box.paddings.reverse().map((item) => {
+            item.l = temp.l - item.l;
+            item.b = temp.b - item.b;
+            temp = {l: item.l, b: item.b};
+            return item;
+        });
+        box.paddings = axesPadd.reverse();
+
+        var wrapperNode = utilsDraw.applyNodeDefaults({
+            type: 'COORDS.RECT',
+            options: utils.clone(rootNode.options),
+            $matrix: new TMatrix([[[coordNode, elemsNode]]]),
+            guide: {
+                split: false,
+                padding: {
+                    l: globPadd.l,
+                    b: globPadd.b,
+                    r: 0,
+                    t: 0
+                }
+            }
+        });
+
+        traverse(coordMatrix, utils.clone(box), box.depth, (unit, selectorPredicates) => {
+
+            var depth = selectorPredicates.depth;
+
+            if (unit.type !== 'COORDS.RECT') {
+                // display: none;
+                return null;
+            }
+
+            unit.guide.x.hide = !selectorPredicates.lastRow;
+            unit.guide.y.hide = !selectorPredicates.firstCol;
+
+            unit.guide.x.padding = (box.paddings[depth].b + unit.guide.x.padding);
+            unit.guide.y.padding = (box.paddings[depth].l + unit.guide.y.padding);
+
+            var d = (depth > 1) ? 0 : 8;
+            unit.guide.padding.l = d;
+            unit.guide.padding.b = d;
+            unit.guide.padding.r = d;
+            unit.guide.padding.t = d;
+
+            unit.guide.showGridLines = '';
+            return unit;
+        });
+
+        traverse(elemsMatrix, utils.clone(box), box.depth, (unit, selectorPredicates) => {
+
+            var depth = selectorPredicates.depth;
+
+            unit.guide.x.hide = true;
+            unit.guide.y.hide = true;
+
+            unit.guide.x.padding = (box.paddings[depth].b + unit.guide.x.padding);
+            unit.guide.y.padding = (box.paddings[depth].l + unit.guide.y.padding);
+
+            var d = (depth > 1) ? 0 : 8;
+            unit.guide.padding.l = d;
+            unit.guide.padding.b = d;
+            unit.guide.padding.r = d;
+            unit.guide.padding.t = d;
+
+            unit.guide.showGridLines = (depth > 1) ? '' : 'xy';
+            return unit;
+        });
+
+        return fnDefaultLayoutEngine(wrapperNode, domainMixin);
+    },
 
     'SHARE-AXES': (rootNode, domainMixin) => {
 
