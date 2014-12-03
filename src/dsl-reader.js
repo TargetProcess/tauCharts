f83a7ff266a72c07e247509f0c7699ef31c63123
konstantin <krivlenia@targetprocess.com>
2014-12-03 17:58:21 +0300

    add default fields for tooltip

--- src/dsl-reader.js
+++ src/dsl-reader.js
@@ -1,7 +1,6 @@
-
 export class DSLReader {
 
-    constructor (domainMixin, UnitsRegistry) {
+    constructor(domainMixin, UnitsRegistry) {
         this.domain = domainMixin;
         this.UnitsRegistry = UnitsRegistry;
     }
@@ -73,11 +72,15 @@ export class DSLReader {
     renderGraph(styledGraph, target, chart) {
         styledGraph.options.container = target;
         var renderRecursively = (unit) => {
-            this.UnitsRegistry.get(unit.type).draw(this.domain.mix(unit), renderRecursively);
-            if(chart) {
+            this.UnitsRegistry.get(unit.type).draw(this.domain.mix(unit), function (childUnit) {
+                childUnit.parentUnit = unit;
+                renderRecursively(childUnit);
+            });
+            if (chart) {
                 chart.fire('unitready', unit);
             }
         };
+        styledGraph.parentUnit = null;
         renderRecursively(styledGraph);
         return styledGraph.options.container;
     }
