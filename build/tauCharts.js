d687af20c7a723e1af542cf21697bcd5313fd3e3
konstantin <krivlenia@targetprocess.com>
2014-12-16 13:55:24 +0300

    release 0.2.10

--- build/tauCharts.js
+++ build/tauCharts.js
@@ -1,4 +1,4 @@
-/*! tauCharts - v0.2.9 - 2014-12-16
+/*! tauCharts - v0.2.10 - 2014-12-16
 * https://github.com/TargetProcess/tauCharts
 * Copyright (c) 2014 Taucraft Limited; Licensed Creative Commons */
 (function (root, factory) {
