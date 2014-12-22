bf0dd17ab70b442c210b5a0ac3ae19bf79e96371
Vladimir Petriko <vlad@targetprocess.com>
2014-12-22 18:25:59 +0300

    BUG#94393 Check all div since d3.select doesn't work on travis for some reason

--- test/utils-draw.test.js
+++ test/utils-draw.test.js
@@ -43,26 +43,53 @@ define(function (require) {
         it("should wrap text", function () {
             var d3Text = d3.select(div).selectAll('text.wrap');
             utilsDraw.wrapText(d3Text, 100, 3, 10, true, textLenMeasurer);
-            expect(d3Text.node().innerHTML).to.equal([
+            expect(div.innerHTML).to.equal([
+                '<div id="test-div" style="width: 800px; height: 600px">',
+                '<svg>',
+                '<text class="long" x="0" y="0" dy="10">0123456789ABCDEFGH</text>',
+                '<text class="word" x="0" y="0" dy="10">012345 6789A BCDEFGH</text>',
+                '<text class="wrap" x="0" y="0" dy="10">',
                 '<tspan x="0" y="-10" dy="10em">Lorem ipsum</tspan>',
                 '<tspan x="0" y="-10" dy="11.1em">dolor sit</tspan>',
-                '<tspan x="0" y="-10" dy="12.2em">amet, consec...</tspan>'
+                '<tspan x="0" y="-10" dy="12.2em">amet, consec...</tspan>',
+                '</text>',
+                '<text class="longwrap" x="0" y="0" dy="10">0123456789ABCDEFGH 0123456789ABCDEFGH</text>',
+                '</svg>',
+                '</div>'
             ].join(''));
         });
 
         it("should wrap continuous text", function () {
             var d3Text = d3.select(div).selectAll('text.long');
             utilsDraw.wrapText(d3Text, 100, 3, 10, true, textLenMeasurer);
-            expect(d3Text.node().innerHTML).to.equal([
-                '<tspan x="0" y="0" dy="10em">0123456789AB...</tspan>'
+            expect(div.innerHTML).to.equal([
+                '<div id="test-div" style="width: 800px; height: 600px">',
+                '<svg>',
+                '<text class="long" x="0" y="0" dy="10">',
+                '<tspan x="0" y="0" dy="10em">0123456789AB...</tspan>',
+                '</text>',
+                '<text class="word" x="0" y="0" dy="10">012345 6789A BCDEFGH</text>',
+                '<text class="wrap" x="0" y="0" dy="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit</text>',
+                '<text class="longwrap" x="0" y="0" dy="10">0123456789ABCDEFGH 0123456789ABCDEFGH</text>',
+                '</svg>',
+                '</div>'
             ].join(''));
         });
 
         it("should wrap several continuous text tokens", function () {
             var d3Text = d3.select(div).selectAll('text.longwrap');
             utilsDraw.wrapText(d3Text, 100, 3, 10, true, textLenMeasurer);
-            expect(d3Text.node().innerHTML).to.equal([
-                '<tspan x="0" y="0" dy="10em">0123456789AB...</tspan>'
+            expect(div.innerHTML).to.equal([
+                '<div id="test-div" style="width: 800px; height: 600px">',
+                '<svg>',
+                '<text class="long" x="0" y="0" dy="10">0123456789ABCDEFGH</text>',
+                '<text class="word" x="0" y="0" dy="10">012345 6789A BCDEFGH</text>',
+                '<text class="wrap" x="0" y="0" dy="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit</text>',
+                '<text class="longwrap" x="0" y="0" dy="10">',
+                '<tspan x="0" y="0" dy="10em">0123456789AB...</tspan>',
+                '</text>',
+                '</svg>',
+                '</div>'
             ].join(''));
         });
     });
