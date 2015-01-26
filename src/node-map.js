c16d7759149f524b84157cbf5d49dd8a5bfcb8e7
Vladimir Petriko <vlad@targetprocess.com>
2015-01-26 18:05:59 +0300

    set max relative limit to 3.5%

--- src/node-map.js
+++ src/node-map.js
@@ -6,9 +6,9 @@ import {utilsDraw} from './utils/utils-draw';
 import {CoordsParallel} from './elements/coords-parallel';
 import {CoordsParallelLine} from './elements/coords-parallel-line';
 
-var fitSize = (w, h, maxRel, srcSize, minimalSize) => {
+var fitSize = (w, h, maxRelLimit, srcSize, minimalSize) => {
     var minRefPoint = Math.min(w, h);
-    var minSize = minRefPoint * maxRel;
+    var minSize = minRefPoint * maxRelLimit;
     return Math.max(minimalSize, Math.min(srcSize, minSize));
 };
 
@@ -33,16 +33,17 @@ var setupElementNode = (node, dimensions) => {
     node.options.color = node.scaleColor(node.color.scaleDim, guideColor.brewer, guideColor);
 
     if (node.size) {
-        var minimalSize = 2;
+        var minimalSize = 1;
+        var maxRelLimit = 0.035;
         var minFontSize = _.min([node.guide.x.tickFontHeight, node.guide.y.tickFontHeight].filter((x) => x !== 0)) * 0.5;
         var minTickStep = _.min([node.guide.x.density, node.guide.y.density].filter((x) => x !== 0)) * 0.5;
         var guideSize = node.guide.size || {};
         node.options.sizeScale = node.scaleSize(
             node.size.scaleDim,
             [
-                minimalSize,
-                fitSize(W, H, 0.1, minTickStep, minimalSize),
-                fitSize(W, H, 0.1, minFontSize, minimalSize)
+                fitSize(W, H, maxRelLimit, 2, minimalSize),
+                fitSize(W, H, maxRelLimit, minTickStep, minimalSize),
+                fitSize(W, H, maxRelLimit, minFontSize, minimalSize)
             ],
             guideSize);
     }
