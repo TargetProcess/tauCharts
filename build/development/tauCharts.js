31e755718ff76731de8cb53f3347ee11b2cdf2c3
konstantin <krivlenia@targetprocess.com>
2015-01-29 17:11:47 +0300

    Updates

--- build/development/tauCharts.js
+++ build/development/tauCharts.js
@@ -1,4 +1,4 @@
-/*! taucharts - v0.3.15 - 2015-01-28
+/*! taucharts - v0.3.15 - 2015-01-29
 * https://github.com/TargetProcess/tauCharts
 * Copyright (c) 2015 Taucraft Limited; Licensed Apache License 2.0 */
 (function (root, factory) {
