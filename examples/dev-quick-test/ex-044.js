0f24721a88c003df9b6ff5eb9d53846da5683c08
vladminsky <vlad.minsky@gmail.com>
2015-11-30 16:07:26 +0300

    Support right "scaleOrient"

---
+++ examples/dev-quick-test/ex-044.js
@@ -0,0 +1,59 @@
+var now = new Date();
+function days (i) {
+    return (new Date(now - i * 1000 * 60 * 60 * 24));
+}
+
+window.samples.push({
+
+    data: [
+        {z: 'A', x: days(7), reposts: 10, likes: 21, comments: 10, followers: 100},
+        {z: 'A', x: days(6),              likes: 20, comments: 7 , followers: 110},
+        {z: 'A', x: days(5), reposts: 21, likes: 12, comments: 21, followers: 120},
+        {z: 'B', x: days(4), reposts: 14,            comments: 31, followers: 140},
+        {z: 'B', x: days(3), reposts: 11, likes: 42, comments: 10, followers: 170},
+        {z: 'B', x: days(2), reposts: 2 , likes: 10, comments: 11, followers: 177}
+    ],
+    settings: {
+        specEngine: 'none'
+    },
+    guide: {
+        padding: {
+            l: 80,
+            t: 10,
+            r: 80,
+            b: 80
+        },
+        x: {
+            label: {
+                text: 'XXX',
+                padding: 50
+            },
+            textAnchor: 'start',
+            padding: 10,
+            rotate: 45,
+            tickFormat: 'x-time-auto'
+        },
+        y: {
+            label: {
+                text:'Followers',
+                padding: -45
+            },
+            padding: 10,
+            textAnchor: 'start',
+            rotate: 0,
+            tickFormat: 'x-num-auto',
+            scaleOrient: 'right'
+        },
+        showGridLines: 'xy'
+    },
+    type: 'line',
+    x: ['x'],
+    y: ['followers'],
+    color: 'z',
+    plugins: [
+        tauCharts.api.plugins.get('legend')()
+        ,
+        tauCharts.api.plugins.get('tooltip')()
+    ]
+
+});
