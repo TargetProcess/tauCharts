49c6f82ef3c10c9343f7226bb02e1cb4331e82b3
vladminsky <vlad.minsky@gmail.com>
2016-02-23 17:45:05 +0300

    Fix https://github.com/TargetProcess/tauCharts/issues/76
    Add test sample
    
    Don't rely on w/h of element. It may vary in non-regular facets
    
    Test for issue case
    
    Set low limit for radius
    
    Test on size guide customization

---
+++ examples/dev-quick-test/ex-049.js
@@ -0,0 +1,51 @@
+window.samples.push({
+    "type": "scatterplot",
+    "x": ["x1"],
+    "y": ["y1", "y2"],
+    data: [{
+            x1: 1,
+            y1: 'BIG',
+            y2: 'A'
+        }, {
+            x1: 2,
+            y1: 'BIG',
+            y2: 'B'
+        }, {
+            x1: 3,
+            y1: 'BIG',
+            y2: 'C'
+        }, {
+            x1: 4,
+            y1: 'BIG',
+            y2: 'D'
+        }, {
+            x1: 5,
+            y1: 'BIG',
+            y2: 'E'
+        }, {
+            x1: 6,
+            y1: 'BIG',
+            y2: 'F'
+        }, {
+            x1: 7,
+            y1: 'BIG',
+            y2: 'G'
+        }, {
+            x1: 8,
+            y1: 'BIG',
+            y2: 'H'
+        }, {
+            x1: 9,
+            y1: 'BIG',
+            y2: 'I'
+        }, {
+            x1: 10,
+            y1: 'BIG',
+            y2: 'K'
+        }, {
+            x1: 3,
+            y1: 'SMALL',
+            y2: 'Z'
+        }
+    ]
+});
--- test/chart-scatterplot.test.js
+++ test/chart-scatterplot.test.js
@@ -20,4 +20,79 @@ define(function (require) {
             assert.equal(schemes.scatterplotGPL.errors(scatter.getSpec()), false, 'spec right');
         })
     });
+
+    describe('facet scatter plot chart', function () {
+
+        var element;
+        beforeEach(function () {
+            element = document.createElement('div');
+            document.body.appendChild(element);
+        });
+
+        afterEach(function () {
+            element.parentNode.removeChild(element);
+        });
+
+        it('point size should not depend on facet segment size', function () {
+            var scatter = new tauChart.Chart({
+                type: 'scatterplot',
+                x: ['x1'],
+                y: ['y1', 'y2'],
+                data: [
+                    {x1: 1, y1: 'BIG', y2: 'A'},
+                    {x1: 2, y1: 'BIG', y2: 'B'},
+                    {x1: 3, y1: 'BIG', y2: 'C'},
+                    {x1: 4, y1: 'BIG', y2: 'D'},
+                    {x1: 5, y1: 'BIG', y2: 'E'},
+                    {x1: 6, y1: 'BIG', y2: 'F'},
+                    {x1: 7, y1: 'BIG', y2: 'G'},
+                    {x1: 8, y1: 'BIG', y2: 'H'},
+                    {x1: 9, y1: 'BIG', y2: 'I'},
+                    {x1: 3, y1: 'SMALL', y2: 'Z'}
+                ]
+            });
+            scatter.renderTo(element, {width: 800, height: 800});
+
+            var dots = d3.select(element).selectAll('.dot');
+            assert.equal(dots.size(), 10);
+            var radiusArr = dots[0].map((node) => d3.select(node).attr('r'));
+            assert.equal(radiusArr.every(x => x === radiusArr[0]), true, 'all radius are equal');
+        });
+
+        it('should allow to customize point size', function () {
+            var scatter = new tauChart.Chart({
+                type: 'scatterplot',
+                x: ['x1'],
+                y: ['y1', 'y2'],
+                data: [
+                    {x1: 1, y1: 'BIG', y2: 'A'},
+                    {x1: 2, y1: 'BIG', y2: 'B'},
+                    {x1: 3, y1: 'BIG', y2: 'C'},
+                    {x1: 4, y1: 'BIG', y2: 'D'},
+                    {x1: 5, y1: 'BIG', y2: 'E'},
+                    {x1: 6, y1: 'BIG', y2: 'F'},
+                    {x1: 7, y1: 'BIG', y2: 'G'},
+                    {x1: 8, y1: 'BIG', y2: 'H'},
+                    {x1: 9, y1: 'BIG', y2: 'I'},
+                    {x1: 3, y1: 'SMALL', y2: 'Z'}
+                ],
+                guide: [
+                    {},
+                    {
+                        size: {
+                            min: 5,
+                            max: 22,
+                            mid: 27
+                        }
+                    }
+                ]
+            });
+            scatter.renderTo(element, {width: 800, height: 800});
+
+            var dots = d3.select(element).selectAll('.dot');
+            assert.equal(dots.size(), 10);
+            var radiusArr = dots[0].map((node) => d3.select(node).attr('r'));
+            assert.equal(radiusArr.every(x => x == 27), true, 'all radius are equal');
+        })
+    });
 });
