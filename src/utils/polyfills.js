4c7ca269aee592bc08cca590abaaf15fbf838be1
Juanjo Diaz <juanjo.diazmo@gmail.com>
2016-09-12 22:09:47 +0300

    Use Object.defineProperty in polyfill to avoid enumerable polyfilled properties.

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -2,58 +2,82 @@ window.requestAnimationFrame = window.requestAnimationFrame
     || window.webkitRequestAnimationFrame
     || (function (fn) { setTimeout(fn, 17); });
 
-Number.isFinite = Number.isFinite || function(value) {
-    return typeof value === 'number' && isFinite(value);
-};
+if (!Number.isFinite) {
+    Object.defineProperty(Number, 'isFinite', {
+        value: function (value) {
+            return typeof value === 'number' && isFinite(value);
+        },
+        configurable: true,
+        enumerable: false,
+        writable: true
+    });
+}
 
-Number.isNaN = Number.isNaN || function(value) {
-    return typeof value === 'number' && isNaN(value);
-};
+if (!Number.isNaN) {
+    Object.defineProperty(Number, 'isNaN', {
+        value: function (value) {
+            return typeof value === 'number' && isNaN(value);
+        },
+        configurable: true,
+        enumerable: false,
+        writable: true
+    });
+}
 
 if (!Array.prototype.find) {
-    Array.prototype.find = function (predicate) {
-        'use strict';
-        if (this == null) {
-            throw new TypeError('Array.prototype.find called on null or undefined');
-        }
-        if (typeof predicate !== 'function') {
-            throw new TypeError('predicate must be a function');
-        }
-        var list = Object(this);
-        var length = list.length >>> 0;
-        var thisArg = arguments[1];
-        var value;
-
-        for (var i = 0; i < length; i++) {
-            value = list[i];
-            if (predicate.call(thisArg, value, i, list)) {
-                return value;
+    Object.defineProperty(Array.prototype, 'find', {
+        value: function (predicate) {
+            'use strict';
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
             }
-        }
-        return undefined;
-    };
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                value = list[i];
+                if (predicate.call(thisArg, value, i, list)) {
+                    return value;
+                }
+            }
+            return undefined;
+        },
+        configurable: true,
+        enumerable: false,
+        writable: true
+    });
 }
 
 if (!Array.prototype.findIndex) {
-    Array.prototype.findIndex = function (predicate) {
-        'use strict';
-        if (this == null) {
-            throw new TypeError('Array.prototype.findIndex called on null or undefined');
-        }
-        if (typeof predicate !== 'function') {
-            throw new TypeError('predicate must be a function');
-        }
-        var list = Object(this);
-        var length = list.length >>> 0;
-        var thisArg = arguments[1];
-        var value;
-
-        for (var i = 0; i < length; i++) {
-            value = list[i];
-            if (predicate.call(thisArg, value, i, list)) {
-                return i;
+    Object.defineProperty(Array.prototype, 'findIndex', {
+        value: function (predicate) {
+            'use strict';
+            if (this == null) {
+                throw new TypeError('Array.prototype.findIndex called on null or undefined');
             }
-        }
-        return -1;
-    };
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                value = list[i];
+                if (predicate.call(thisArg, value, i, list)) {
+                    return i;
+                }
+            }
+            return -1;
+        },
+        configurable: true,
+        enumerable: false,
+        writable: true
+    });
 }
