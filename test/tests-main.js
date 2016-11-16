a9a0516dff38c66da9b09ace437715914f25753d
Alexander Shutov <shutovby@gmail.com>
2016-11-16 13:33:25 +0300

    Setup font for tests.

--- test/tests-main.js
+++ test/tests-main.js
@@ -5,6 +5,13 @@ tauCharts.api.globalSettings.animationSpeed = 0;
 tauCharts.api.globalSettings.renderingTimeout = 0;
 tauCharts.api.globalSettings.asyncRendering = false;
 
+// Setup font for tests
+(function () {
+    var s = document.createElement('style');
+    s.textContent = '* { font-family: Arial !important; }';
+    document.head.appendChild(s);
+})();
+
 var tests = [];
 for (var file in window.__karma__.files) {
     if (/test.js$/.test(file)) {
