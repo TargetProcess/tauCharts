bc5880d5e6d7760fea345532e032ae939b67e933
Alexander Shutov <shutovby@gmail.com>
2016-12-06 19:02:42 +0300

    Refactored splitting cubic segment.

--- src/utils/path/svg/brush-line.js
+++ src/utils/path/svg/brush-line.js
@@ -1,4 +1,4 @@
-import {bezier, getBezierPoint} from '../bezier';
+import {bezier, getBezierPoint, splitCubicSegment as split} from '../bezier';
 
 /**
  * Returns line with variable width.
@@ -52,7 +52,7 @@ export function getBrushCurve(points) {
 /**
  * Returns single circle as part of SVG path.
  */
-function getCircle(x, y, r) {
+function getCirclePath(x, y, r) {
     return [
         `M${x},${y - r}`,
         `A${r},${r} 0 0 1`,
@@ -64,47 +64,40 @@ function getCircle(x, y, r) {
 }
 
 /**
+ * Returns two circles joined with line path.
+ */
+function getStraightSegmentPath(a, b, tan) {
+    return [
+        `M${tan.a.left.x},${tan.a.left.y}`,
+        `L${tan.b.left.x},${tan.b.left.y}`,
+        `A${b.size / 2},${b.size / 2} 0 ${Number(a.size < b.size)} 1`,
+        `${tan.b.right.x},${tan.b.right.y}`,
+        `L${tan.a.right.x},${tan.a.right.y}`,
+        `A${a.size / 2},${a.size / 2} 0 ${Number(a.size > b.size)} 1`,
+        `${tan.a.left.x},${tan.a.left.y}`,
+        'Z'
+    ].join(' ');
+}
+
+/**
  * Returns two circles joined with tangents.
  */
 function getSegment(a, b) {
-    var mainDistance = getDistance(a, b);
-    if (mainDistance === 0 ||
-        (mainDistance + a.size / 2 <= b.size / 2) ||
-        (mainDistance + b.size / 2 <= a.size / 2)
-    ) {
+    var tan = getCirclesTangents(a, b);
+    if (!tan) {
         // Return single circle, if one is inside another
         var largerPt = a.size > b.size ? a : b;
         var radius = largerPt.size / 2;
-        return getCircle(largerPt.x, largerPt.y, radius);
+        return getCirclePath(largerPt.x, largerPt.y, radius);
     }
-
-    var mainAngle = getAngle(a, b);
-    var tangentAngle = Math.asin((a.size - b.size) / mainDistance / 2);
-    var angleLeft = mainAngle - Math.PI / 2 + tangentAngle;
-    var angleRight = mainAngle + Math.PI / 2 - tangentAngle;
-
-    var tangentLeftA = getPolarPoint(a, a.size / 2, angleLeft);
-    var tangentLeftB = getPolarPoint(b, b.size / 2, angleLeft);
-    var tangentRightA = getPolarPoint(a, a.size / 2, angleRight);
-    var tangentRightB = getPolarPoint(b, b.size / 2, angleRight);
-
-    return [
-        `M${tangentLeftA.x},${tangentLeftA.y}`,
-        `L${tangentLeftB.x},${tangentLeftB.y}`,
-        `A${b.size / 2},${b.size / 2} 0 ${Number(tangentAngle < 0)} 1`,
-        `${tangentRightB.x},${tangentRightB.y}`,
-        `L${tangentRightA.x},${tangentRightA.y}`,
-        `A${a.size / 2},${a.size / 2} 0 ${Number(tangentAngle > 0)} 1`,
-        `${tangentLeftA.x},${tangentLeftA.y}`,
-        'Z'
-    ].join(' ');
+    return getStraightSegmentPath(a, b, tan);
 }
 
 /**
  * Returns two circles joined with cubic curves.
  */
 function getCurveSegment(a, c1, c2, b) {
-    var mainDistance = getDistance(a, b);
+    var mainDistance = dist(a, b);
     if (mainDistance === 0 ||
         (mainDistance + a.size / 2 <= b.size / 2) ||
         (mainDistance + b.size / 2 <= a.size / 2)
@@ -112,7 +105,7 @@ function getCurveSegment(a, c1, c2, b) {
         // Return single circle, if one is inside another
         var largerPt = a.size > b.size ? a : b;
         var radius = largerPt.size / 2;
-        return getCircle(largerPt.x, largerPt.y, radius);
+        return getCirclePath(largerPt.x, largerPt.y, radius);
     }
 
     // NOTE: Replace self-intersected segment with straight.
@@ -120,37 +113,37 @@ function getCurveSegment(a, c1, c2, b) {
         return getSegment(a, b);
     }
 
-    var mainAngle = getAngle(a, b);
+    var mainAngle = angle(a, b);
     var tangentAngle = Math.asin((a.size - b.size) / mainDistance / 2);
     var angleLeft = mainAngle - Math.PI / 2 + tangentAngle;
     var angleRight = mainAngle + Math.PI / 2 - tangentAngle;
 
-    var angleA = getAngle(a, c1);
-    var angleB = getAngle(c2, b);
+    var angleA = angle(a, c1);
+    var angleB = angle(c2, b);
 
-    var tangentLeftA = getPolarPoint(a, a.size / 2, angleLeft + angleA - mainAngle);
-    var tangentLeftB = getPolarPoint(b, b.size / 2, angleLeft + angleB - mainAngle);
-    var tangentRightA = getPolarPoint(a, a.size / 2, angleRight + angleA - mainAngle);
-    var tangentRightB = getPolarPoint(b, b.size / 2, angleRight + angleB - mainAngle);
+    var tangentLeftA = polar(a, a.size / 2, angleLeft + angleA - mainAngle);
+    var tangentLeftB = polar(b, b.size / 2, angleLeft + angleB - mainAngle);
+    var tangentRightA = polar(a, a.size / 2, angleRight + angleA - mainAngle);
+    var tangentRightB = polar(b, b.size / 2, angleRight + angleB - mainAngle);
 
-    var cLeftA = getPolarPoint(
+    var cLeftA = polar(
         tangentLeftA,
-        getDistance(a, c1) / mainDistance * getDistance(tangentLeftA, tangentLeftB),
+        dist(a, c1) / mainDistance * dist(tangentLeftA, tangentLeftB),
         angleLeft + angleA - mainAngle + Math.PI / 2
     );
-    var cLeftB = getPolarPoint(
+    var cLeftB = polar(
         tangentLeftB,
-        getDistance(c2, b) / mainDistance * getDistance(tangentLeftA, tangentLeftB),
+        dist(c2, b) / mainDistance * dist(tangentLeftA, tangentLeftB),
         angleLeft + angleB - mainAngle - Math.PI / 2
     );
-    var cRightA = getPolarPoint(
+    var cRightA = polar(
         tangentRightA,
-        getDistance(a, c1) / mainDistance * getDistance(tangentRightA, tangentRightB),
+        dist(a, c1) / mainDistance * dist(tangentRightA, tangentRightB),
         angleRight + angleA - mainAngle - Math.PI / 2
     );
-    var cRightB = getPolarPoint(
+    var cRightB = polar(
         tangentRightB,
-        getDistance(c2, b) / mainDistance * getDistance(tangentRightA, tangentRightB),
+        dist(c2, b) / mainDistance * dist(tangentRightA, tangentRightB),
         angleRight + angleB - mainAngle + Math.PI / 2
     );
 
@@ -170,35 +163,66 @@ function getCurveSegment(a, c1, c2, b) {
     ].join(' ');
 }
 
-function getAngle(a, b) {
+function angle(a, b) {
     return Math.atan2(b.y - a.y, b.x - a.x);
 }
 
-function getDistance(a, b) {
-    return Math.sqrt((b.y - a.y) * (b.y - a.y) + (b.x - a.x) * (b.x - a.x));
+function dist(...p) {
+    var total = 0;
+    for (var i = 1; i < p.length; i++) {
+        total += Math.sqrt(
+            (p[i].x - p[i - 1].x) * (p[i].x - p[i - 1].x) +
+            (p[i].y - p[i - 1].y) * (p[i].y - p[i - 1].y)
+        );
+    }
+    return total;
 }
 
-function getPolarPoint(start, distance, angle) {
+function polar(start, d, a) {
     return {
-        x: start.x + distance * Math.cos(angle),
-        y: start.y + distance * Math.sin(angle)
+        x: start.x + d * Math.cos(a),
+        y: start.y + d * Math.sin(a)
     };
 }
 
 function splitCurveSegment(p0, c0, c1, p1) {
-    var c2 = getBezierPoint(0.5, p0, c0);
-    var c3 = getBezierPoint(0.5, p0, c0, c1);
-    var r = Object.keys(p1)
-        .reduce((memo, k) => {
-            if (k === 'x' || k === 'y') {
-                memo[k] = bezier(0.5, p0[k], c0[k], c1[k], p1[k]);
-            } else if (k === 'size') {
-                memo.size = (p0.size + p1.size) / 2;
-            }
-            return memo;
-        }, {});
-    var c4 = getBezierPoint(0.5, c0, c1, p1);
-    var c5 = getBezierPoint(0.5, c1, p1);
+    var seg = split(0.5, p0, c0, c1, p1);
+    var tl = 1 / (1 +
+        dist(seg[3], seg[4], seg[5], seg[6], seg[3]) /
+        dist(seg[0], seg[1], seg[2], seg[3], seg[0])
+    );
+    seg[3].size = p0.size * (1 - tl) + p1.size * tl;
 
-    return [p0, c2, c3, r, c4, c5, p1];
+    return seg;
+}
+
+function getCirclesTangents(a, b) {
+    var mainDistance = dist(a, b);
+    if (mainDistance === 0 ||
+        (mainDistance + a.size / 2 <= b.size / 2) ||
+        (mainDistance + b.size / 2 <= a.size / 2)
+    ) {
+        return null;
+    }
+
+    var mainAngle = angle(a, b);
+    var tangentAngle = Math.asin((a.size - b.size) / mainDistance / 2);
+    var angleLeft = mainAngle - Math.PI / 2 + tangentAngle;
+    var angleRight = mainAngle + Math.PI / 2 - tangentAngle;
+
+    var tangentLeftA = polar(a, a.size / 2, angleLeft);
+    var tangentLeftB = polar(b, b.size / 2, angleLeft);
+    var tangentRightA = polar(a, a.size / 2, angleRight);
+    var tangentRightB = polar(b, b.size / 2, angleRight);
+
+    return {
+        a: {
+            left: polar(a, a.size / 2, angleLeft),
+            right: polar(a, a.size / 2, angleRight)
+        },
+        b: {
+            left: polar(b, b.size / 2, angleLeft),
+            right: polar(b, b.size / 2, angleRight)
+        }
+    };
 }
