d09d2cea1dc02ba5d624437988595c80acd7f54d
Alexander Shutov <shutovby@gmail.com>
2016-12-14 14:19:05 +0300

    Minimalistic diff design.

--- examples/specs/bar-labels.js
+++ examples/specs/bar-labels.js
@@ -78,30 +78,21 @@
 
                             var decreasing = (Number(attr0.height) > Number(attr1.height));
 
-                            var patternId = ('diff__pattern-' + id + '-' + gi + '-' + i);
-                            var pattern = defs.append('pattern')
-                                .attr('id', patternId)
-                                .attr('patternUnits', 'userSpaceOnUse')
-                                .attr('x', 0)
-                                .attr('y', 0)
-                                .attr('width', 16)
-                                .attr('height', 16);
-
                             if (decreasing) {
-                                bar0.setAttribute('x', attr0.x);
+                                bar0.setAttribute('x', attr0.x + 1);
                                 bar0.setAttribute('y', attr0.y);
-                                bar0.setAttribute('width', attr0.width);
-                                bar0.setAttribute('height', String(attr0.height - attr1.height));
+                                bar0.setAttribute('width', attr0.width - 2);
+                                bar0.setAttribute('height', String(attr0.height - attr1.height) + 1);
                                 bar1.setAttribute('x', attr1.x);
                                 bar1.setAttribute('y', attr1.y);
                                 bar1.setAttribute('width', attr1.width);
                                 bar1.setAttribute('height', attr1.height);
 
-                                pattern.append('path')
-                                    .attr('d', 'M0,1 v14 l1,1 h14 Z M16,15 v-14 l-1,-1 h-14 Z')
-                                    .attr('opacity', 0.125)
-                                    .attr('fill', d3.interpolate(fill, 'red')(0.5));
-                                d3.select(bar0).style('fill', 'url(#' + patternId + ')');
+                                d3.select(bar0)
+                                    .style('fill', 'none')
+                                    .style('stroke', fill)
+                                    .style('stroke-width', 1)
+                                    .style('stroke-dasharray', '2 1');
                             } else {
                                 bar0.setAttribute('x', attr0.x);
                                 bar0.setAttribute('y', attr0.y);
@@ -111,12 +102,10 @@
                                 bar1.setAttribute('y', attr1.y);
                                 bar1.setAttribute('width', attr1.width);
                                 bar1.setAttribute('height', String(attr1.height - attr0.height));
-
-                                pattern.append('path')
-                                    .attr('d', 'M0,15 v-14 l1,-1 h14 Z M16,1 v14 l-1,1 h-14 Z')
-                                    .attr('fill', fill);
-                                d3.select(bar1).style('fill', 'url(#' + patternId + ')');
                             }
+
+                            d3.select(bar0).style('shape-rendering', 'chrispEdges');
+                            d3.select(bar1).style('shape-rendering', 'chrispEdges');
 
                             d3.select(bar0).transition();
                             d3.select(bar1).transition();
@@ -130,13 +119,8 @@
 
                             if (label0 && label1) {
 
-                                var labelToHide = label0;
-                                var labelToShow = label1;
-
-                                labelToShow.textContent = (
-                                    (decreasing ? '▼' : '▲') +
-                                    (label0.textContent === '0' ? '100' : Math.round(Math.abs(label1.textContent - label0.textContent) / label0.textContent * 100)) + '%'
-                                );
+                                var labelToHide = Number(label0.textContent) > Number(label1.textContent) ? label1 : label0;
+                                var labelToShow = Number(label0.textContent) > Number(label1.textContent) ? label0 : label1;
 
                                 labelToShow.setAttribute('y', -5);
 
