233c5ff9f1fd2ceee724d85926d8890dc18408f7
Alexander Shutov <shutovby@gmail.com>
2017-01-04 11:54:22 +0300

    Tests fo correct bar labels placement.

--- test/chart-bar.test.js
+++ test/chart-bar.test.js
@@ -42,13 +42,13 @@ define(function(require){
         it('should draw labels after bars', function () {
             var testDiv = createTestDiv();
             var data1 = [
-                { x: 10, y: 10, t: 'a' },
-                { x: 10, y: 20, t: 'b' },
-                { x: 20, y: 15, t: 'c' }
+                {x: 10, y: 10, t: 'a'},
+                {x: 10, y: 20, t: 'b'},
+                {x: 20, y: 15, t: 'c'}
             ];
             var data2 = [
-                { x: 10, y: 20, t: 'b' },
-                { x: 20, y: 15, t: 'c' }
+                {x: 10, y: 20, t: 'b'},
+                {x: 20, y: 15, t: 'c'}
             ];
             var bar = new tauChart.Chart({
                 type: 'horizontal-bar',
@@ -73,6 +73,96 @@ define(function(require){
                     return b.matches('.i-role-label');
                 });
             assert.equal(lastBarIndex + 1, firstLabelIndex, 'last bar and first label indices');
+            bar.destroy();
+            removeTestDiv();
+        });
+
+        it('should put labels inside and outside of horizontal bars', function () {
+            var toArray = (obj) => Array.prototype.slice.call(obj, 0);
+            var testDiv = createTestDiv();
+            var data = [
+                {x: 10, y: 10, l: 'Anger & swearing - Difficult workaround'},
+                {x: 20, y: 20, l: 'Confused'},
+                {x: -20, y: 30, l: 'Much anger & crying - No workaround'},
+                {x: -10, y: 40, l: 'Laugh if notice'}
+            ];
+            var bar = new tauChart.Chart({
+                type: 'horizontal-bar',
+                data: data,
+                x: 'x',
+                y: 'y',
+                label: 'l'
+            });
+            bar.renderTo(testDiv);
+            var svg = bar.getSVG();
+            var grid = svg.querySelector('.grid').getBoundingClientRect();
+            var bars = toArray(svg.querySelectorAll('.i-role-bar-group rect'))
+                .map(el => el.getBoundingClientRect());
+            var labels = toArray(svg.querySelectorAll('.i-role-label'))
+                .map(el => el.getBoundingClientRect());
+
+            assert.equal(bars.length, data.length);
+            assert.equal(labels.length, data.length);
+
+            assert.equal(labels.every(l => (
+                (l.left >= grid.left) &&
+                (l.right <= grid.right)
+            )), true, 'labels do not exceed grid');
+
+            assert.equal(labels.every(l => {
+                // Find a bar on the same level as label
+                var b = bars.filter(b => l.top > b.top && l.bottom < b.bottom)[0];
+                return !(
+                    (l.left < b.left && l.right > b.left) ||
+                    (l.left < b.right && l.right > b.right)
+                );
+            }), true, 'labels do not intersect bars');
+
+            bar.destroy();
+            removeTestDiv();
+        });
+
+        it('should put labels inside and outside of vertical bars', function () {
+            var toArray = (obj) => Array.prototype.slice.call(obj, 0);
+            var testDiv = createTestDiv();
+            var data = [
+                {x: 10, y: 10, l: 'Anger & swearing - Difficult workaround'},
+                {x: 20, y: 20, l: 'Confused'},
+                {x: -20, y: 30, l: 'Much anger & crying - No workaround'},
+                {x: -10, y: 40, l: 'Laugh if notice'}
+            ];
+            var bar = new tauChart.Chart({
+                type: 'bar',
+                data: data,
+                x: 'y',
+                y: 'x',
+                label: 'l'
+            });
+            bar.renderTo(testDiv);
+            var svg = bar.getSVG();
+            var grid = svg.querySelector('.grid').getBoundingClientRect();
+            var bars = toArray(svg.querySelectorAll('.i-role-bar-group rect'))
+                .map(el => el.getBoundingClientRect());
+            var labels = toArray(svg.querySelectorAll('.i-role-label'))
+                .map(el => el.getBoundingClientRect());
+
+            assert.equal(bars.length, data.length);
+            assert.equal(labels.length, data.length);
+
+            assert.equal(labels.every(l => (
+                (l.top >= grid.top) &&
+                (l.bottom <= grid.bottom)
+            )), true, 'labels do not exceed grid');
+
+            assert.equal(labels.every(l => {
+                // Find a bar on the same level as label
+                var b = bars.filter(b => l.left > b.left && l.right < b.right)[0];
+                return !(
+                    (l.top < b.top && l.bottom > b.top) ||
+                    (l.top < b.bottom && l.bottom > b.bottom)
+                );
+            }), true, 'labels do not intersect bars');
+
             bar.destroy();
             removeTestDiv();
         });
