9f293c2fc968754914a25e86dfb4a95010f2e84b
Alexander Shutov <shutovby@gmail.com>
2017-01-11 19:12:24 +0300

    UTC period scale.

--- src/unit-domain-period-generator.js
+++ src/unit-domain-period-generator.js
@@ -71,16 +71,89 @@ var PERIODS_MAP = {
     }
 };
 
+var PERIODS_MAP_UTC = {
+
+    day: {
+        cast: function (d) {
+            var date = new Date(d);
+            return new Date(date.setUTCHours(0, 0, 0, 0));
+        },
+        next: function (d) {
+            var prev = new Date(d);
+            var next = new Date(prev.setUTCDate(prev.getDate() + 1));
+            return this.cast(next);
+        }
+    },
+
+    week: {
+        cast: function (d) {
+            var date = new Date(d);
+            date = new Date(date.setUTCHours(0, 0, 0, 0));
+            return new Date(date.setUTCDate(date.getDate() - date.getDay()));
+        },
+        next: function (d) {
+            var prev = new Date(d);
+            var next = (new Date(prev.setUTCDate(prev.getDate() + 7)));
+            return this.cast(next);
+        }
+    },
+
+    month: {
+        cast: function (d) {
+            var date = new Date(d);
+            date = new Date(date.setUTCHours(0, 0, 0, 0));
+            date = new Date(date.setUTCDate(1));
+            return date;
+        },
+        next: function (d) {
+            var prev = new Date(d);
+            var next = new Date(prev.setUTCMonth(prev.getMonth() + 1));
+            return this.cast(next);
+        }
+    },
+
+    quarter: {
+        cast: function (d) {
+            var date = new Date(d);
+            date = new Date(date.setUTCHours(0, 0, 0, 0));
+            date = new Date(date.setUTCDate(1));
+            var currentMonth = date.getMonth();
+            var firstQuarterMonth = currentMonth - (currentMonth % 3);
+            return new Date(date.setUTCMonth(firstQuarterMonth));
+        },
+        next: function (d) {
+            var prev = new Date(d);
+            var next = new Date(prev.setUTCMonth(prev.getMonth() + 3));
+            return this.cast(next);
+        }
+    },
+
+    year: {
+        cast(d) {
+            var date = new Date(d);
+            date = new Date(date.setUTCHours(0, 0, 0, 0));
+            date = new Date(date.setUTCDate(1));
+            date = new Date(date.setUTCMonth(0));
+            return date;
+        },
+        next: function (d) {
+            var prev = new Date(d);
+            var next = new Date(prev.setUTCFullYear(prev.getFullYear() + 1));
+            return this.cast(next);
+        }
+    }
+};
+
 var UnitDomainPeriodGenerator = {
 
-    add: function (periodAlias, obj) {
-        PERIODS_MAP[periodAlias.toLowerCase()] = obj;
+    add: function (periodAlias, obj, {utc} = {utc: false}) {
+        (utc ? PERIODS_MAP_UTC : PERIODS_MAP)[periodAlias.toLowerCase()] = obj;
         return this;
     },
 
-    get: (periodAlias) => {
+    get: (periodAlias, {utc} = {utc: false}) => {
         var alias = periodAlias || '';
-        return PERIODS_MAP[alias.toLowerCase()] || null;
+        return (utc ? PERIODS_MAP_UTC : PERIODS_MAP)[alias.toLowerCase()] || null;
     },
 
     generate: (lTick, rTick, periodAlias) => {
