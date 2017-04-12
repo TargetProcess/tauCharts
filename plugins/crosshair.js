6ef51550aa612cb4043589c19f51335af16934be
Alexander Shutov <shutovby@gmail.com>
2017-04-12 10:35:30 +0300

    Display crosshair labels at axes inside boxes.

--- plugins/crosshair.js
+++ plugins/crosshair.js
@@ -15,6 +15,122 @@
     var utils = tauCharts.api.utils;
     var pluginsSDK = tauCharts.api.pluginsSDK;
 
+    function labelBox(options) {
+
+        options = (options || {});
+
+        var node = document.createElementNS(d3.ns.prefix.svg, 'g');
+
+        var g = d3.select(node).attr('class', 'tau-crosshair__label');
+        g.append('rect').attr('class', 'tau-crosshair__label__box');
+        g.append('text').attr('class', 'tau-crosshair__label__text-shadow');
+        g.append('text').attr('class', 'tau-crosshair__label__text');
+
+        var setValues = function (args) {
+
+            var x = args.x;
+            var y = args.y;
+            var text = args.text;
+            var color = args.color;
+            var colorCls = args.colorCls;
+
+            var halign = options.halign;
+            var valign = options.valign;
+            var hpad = options.hpad;
+            var vpad = options.vpad;
+
+            var textAnchor = ({
+                left: 'end',
+                middle: 'middle',
+                right: 'start'
+            }[halign]);
+            var textDx = ({
+                left: -hpad,
+                middle: 0,
+                right: hpad
+            }[halign]);
+
+            var t = g.select('.tau-crosshair__label__text')
+                .attr('class', 'tau-crosshair__label__text ' + colorCls)
+                .attr('fill', color);
+            var tAndBg = g.selectAll('.tau-crosshair__label__text, .tau-crosshair__label__text-shadow')
+                .attr('text-anchor', textAnchor)
+                .attr('x', x + textDx)
+                .attr('y', 0)
+                .text(text);
+            var textBBox = t.node().getBBox();
+            var textDy = ({
+                top: -vpad - textBBox.height / 2,
+                middle: 0,
+                bottom: vpad + textBBox.height / 2
+            }[valign] - textBBox.height / 2 - textBBox.y);
+            tAndBg
+                .attr('y', y + textDy);
+
+            var boxWidth = (textBBox.width + 2 * hpad);
+            var boxHeight = (textBBox.height + 2 * vpad);
+            var boxDx = ({
+                left: -boxWidth,
+                middle: -boxWidth / 2,
+                right: 0
+            }[halign]);
+            var boxDy = ({
+                top: -boxHeight,
+                middle: -boxHeight / 2,
+                bottom: 0
+            }[valign]);
+
+            g.select('.tau-crosshair__label__box')
+                .attr('class', 'tau-crosshair__label__box ' + colorCls)
+                .attr('fill', color)
+                .attr('x', x + boxDx)
+                .attr('y', y + boxDy)
+                .attr('width', boxWidth)
+                .attr('height', boxHeight);
+        };
+
+        var fixOverflow = function () {
+            g.attr('transform', '');
+            var view = options.chart.getLayout().contentContainer.getBoundingClientRect();
+            var svg = options.chart.getSVG().getBoundingClientRect();
+            var label = node.getBoundingClientRect();
+
+            var dx = (
+                Math.max(0, Math.max(view.left, svg.left) - label.left) ||
+                Math.min(0, Math.min(view.right, svg.right) - label.right)
+            );
+            var dy = (
+                Math.max(0, Math.max(view.top, svg.top) - label.top) ||
+                Math.min(0, Math.min(view.bottom, svg.bottom) - label.bottom)
+            );
+            g.attr('transform', 'translate(' + dx + ',' + dy + ')');
+        };
+
+        var instance = {
+
+            options: function (obj) {
+                options = Object.assign(options, obj);
+                return instance;
+            },
+
+            show: function (args) {
+                setValues(args);
+                options.container.appendChild(node);
+                fixOverflow();
+                return instance;
+            },
+
+            hide: function () {
+                if (node.parentNode) {
+                    node.parentNode.removeChild(node);
+                }
+                return instance;
+            }
+        };
+
+        return instance;
+    }
+
     function Crosshair(xSettings) {
 
         var settings = utils.defaults(
@@ -23,8 +139,10 @@
                 xAxis: true,
                 yAxis: true,
                 formatters: {},
-                labelXPadding: 8,
-                labelYPadding: 8
+                labelBoxHPadding: 5,
+                labelBoxVPadding: 3,
+                axisHPadding: 22,
+                axisVPadding: 22
             });
 
         var plugin = {
@@ -34,28 +152,39 @@
                 this._chart = chart;
                 this._formatters = {};
 
-                this._element = this._createNode();
+                this._createNode();
 
             },
 
             _createNode: function () {
-                var node = d3.select(
+
+                var root = d3.select(
                     document.createElementNS(d3.ns.prefix.svg, 'g'))
                     .attr('class', 'tau-crosshair');
+                this._labels = { x: null, y: null };
                 var createAxisNode = function (dir) {
-                    var g = node.append('g').attr('class', 'tau-crosshair__group ' + dir);
-                    g.append('line').attr('class', 'tau-crosshair__line-bg');
+                    var g = root.append('g').attr('class', 'tau-crosshair__group ' + dir);
+                    g.append('line').attr('class', 'tau-crosshair__line-shadow');
                     g.append('line').attr('class', 'tau-crosshair__line');
-                    g.append('text').attr('class', 'tau-crosshair__label-bg');
-                    g.append('text').attr('class', 'tau-crosshair__label');
-                };
+
+                    this._labels[dir] = labelBox({
+                        container: g.node(),
+                        chart: this._chart,
+                        halign: dir === 'x' ? 'middle' : 'left',
+                        valign: dir === 'x' ? 'bottom' : 'middle',
+                        hpad: settings.labelBoxHPadding,
+                        vpad: settings.labelBoxVPadding
+                    });
+                }.bind(this);
+
                 if (settings.xAxis) {
                     createAxisNode('x');
                 }
                 if (settings.yAxis) {
                     createAxisNode('y');
                 }
-                return node;
+
+                this._element = root;
             },
 
             _setValues: function (xData, yData, colorData) {
@@ -67,42 +196,60 @@
                     g.select('.tau-crosshair__line')
                         .attr('class', 'tau-crosshair__line ' + colorData.cls)
                         .attr('stroke', colorData.color);
-                    g.selectAll('.tau-crosshair__line, .tau-crosshair__line-bg')
-                        .attr('x1', data.lineX1)
-                        .attr('x2', data.lineX2)
-                        .attr('y1', data.lineY1)
-                        .attr('y2', data.lineY2);
+                    g.selectAll('.tau-crosshair__line, .tau-crosshair__line-shadow')
+                        .attr('x1', data.startPt.x)
+                        .attr('x2', data.valuePt.x)
+                        .attr('y1', data.startPt.y)
+                        .attr('y2', data.valuePt.y);
 
-                    g.select('.tau-crosshair__label')
-                        .attr('class', 'tau-crosshair__label ' + colorData.cls)
-                        .attr('fill', colorData.color);
-                    g.selectAll('.tau-crosshair__label, .tau-crosshair__label-bg')
-                        .attr('x', data.textX + settings.labelXPadding)
-                        .attr('y', data.textY - settings.labelYPadding)
-                        .text(data.label);
+                    if (
+                        (data.dir === 'x' && settings.xAxis) ||
+                        (data.dir === 'y' && settings.yAxis)
+                    ) {
+                        this._labels[data.dir]
+                            .options({
+                                halign: data.labelHAlign,
+                                valign: data.labelVAlign
+                            })
+                            .show({
+                                x: data.startPt.x,
+                                y: data.startPt.y,
+                                text: data.label,
+                                color: colorData.color,
+                                colorCls: colorData.cls
+                            });
+                    }
 
                 }.bind(this);
 
                 setCrosshairGroupValues({
                     dir: 'x',
-                    lineX1: xData.value,
-                    lineX2: xData.value,
-                    lineY1: yData.value + yData.crossPadding,
-                    lineY2: yData.start,
+                    startPt: {
+                        x: xData.value,
+                        y: yData.start + settings.axisVPadding
+                    },
+                    valuePt: {
+                        x: xData.value,
+                        y: yData.value + yData.crossPadding
+                    },
                     label: xData.label,
-                    textX: xData.value,
-                    textY: yData.start
+                    labelHAlign: 'middle',
+                    labelVAlign: 'bottom'
                 });
 
                 setCrosshairGroupValues({
                     dir: 'y',
-                    lineX1: xData.start,
-                    lineX2: xData.value + xData.crossPadding,
-                    lineY1: yData.value,
-                    lineY2: yData.value,
+                    startPt: {
+                        x: xData.start - settings.axisHPadding,
+                        y: yData.value
+                    },
+                    valuePt: {
+                        x: xData.value - xData.crossPadding,
+                        y: yData.value
+                    },
                     label: yData.label,
-                    textX: xData.start,
-                    textY: yData.value
+                    labelHAlign: 'left',
+                    labelVAlign: 'middle'
                 });
             },
 
@@ -149,12 +296,12 @@
                     if (unit.config.type === 'ELEMENT.INTERVAL' ||
                         unit.config.type === 'ELEMENT.INTERVAL.STACKED') {
                         return {
-                            x: (-box.width * (unit.config.flip ? xValue > 0 ? 1 : 0 : 0.5)),
+                            x: (box.width * (unit.config.flip ? xValue > 0 ? 1 : 0 : 0.5)),
                             y: (box.height * (unit.config.flip ? 0.5 : yValue > 0 ? 1 : 0))
                         };
                     }
                     return {
-                        x: (-box.width / 2),
+                        x: (box.width / 2),
                         y: (box.height / 2)
                     };
                 })();
