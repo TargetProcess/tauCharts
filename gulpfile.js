185ffcbe63a74e8fa2d3f232aad538c28f8f259d
Alexander Shutov <shutovby@gmail.com>
2017-04-14 16:49:32 +0300

    Update dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,9 @@ const plugins = { connect };
 require('./tasks/gulp-js')(gulp, plugins);
 require('./tasks/gulp-css')(gulp, plugins);
 require('./tasks/gulp-watch')(gulp, plugins);
+
+gulp.task('build', [
+    'build-js',
+    'build-plugins-js',
+    'build-css'
+]);
