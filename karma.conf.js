f855a873461cdb4c774c4b15b82678805c7491a0
Alexander Shutov <shutovby@gmail.com>
2017-06-23 18:20:29 +0300

    build css only

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,8 @@
 module.exports = function (config) {
     console.log('dist/', require('fs').readdirSync('./dist'));
     console.log('dist/css', require('fs').readdirSync('./dist/css'));
-    console.log('dist/plugins', require('fs').readdirSync('./dist/plugins'));
-    console.log('/', require('fs').readdirSync('./'));
+    // console.log('dist/plugins', require('fs').readdirSync('./dist/plugins'));
+    // console.log('/', require('fs').readdirSync('./'));
     config.set({
 
         basePath: '.',
