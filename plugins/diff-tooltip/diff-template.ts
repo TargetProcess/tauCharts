ec792260814e38ed820603a8ba366218d6f496f7
Alexander Shutov <shutovby@gmail.com>
2017-08-21 13:20:00 +0300

    Fixed Diff category null alias.

--- plugins/diff-tooltip/diff-template.ts
+++ plugins/diff-tooltip/diff-template.ts
@@ -156,7 +156,7 @@ export default function DiffTemplate(tooltip: ElementTooltip, settings: TooltipS
         tableRowTemplate({data, prev, highlighted, valueField, colorField, min, max}) {
 
             const v = data[valueField];
-            const name = (colorField != null ? this.getLabel(data[colorField]) : 'no group');
+            const name = (colorField != null ? this.getFormatter(colorField)(data[colorField]) : 'no group');
             const format = this.getFormatter(valueField);
             const value = format(v);
             const isHighlighted = (data === highlighted);
