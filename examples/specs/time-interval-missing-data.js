f57b6a746e07a589a31d4ea6fad4461309e8ffc0
Alexander Shutov <shutovby@gmail.com>
2017-10-04 13:31:01 +0300

    Fixed splitting line into negative and positive when filling gaps.

--- examples/specs/time-interval-missing-data.js
+++ examples/specs/time-interval-missing-data.js
@@ -26,19 +26,18 @@ dev.spec({
 
         {team: 'Manchester', date: new Date('2015-03-01'), points: 60},
         {team: 'Chelsea', date: new Date('2015-03-01'), points: 50},
-        {team: 'Aston Villa', date: new Date('2015-03-01'), points: 20},
+        {team: 'Aston Villa', date: new Date('2015-03-01'), points: -20},
 
         {team: 'Manchester', date: new Date('2015-04-01'), points: 60},
         {team: 'Aston Villa', date: new Date('2015-04-01'), points: 10},
 
         {team: 'Manchester', date: new Date('2015-05-01'), points: 50},
-        {team: 'Chelsea', date: new Date('2015-05-01'), points: 40},
+        {team: 'Chelsea', date: new Date('2015-05-01'), points: -40},
         {team: 'Aston Villa', date: new Date('2015-05-01'), points: 10},
 
         {team: 'Manchester', date: new Date('2015-06-01'), points: 30},
 
         {team: 'Manchester', date: new Date('2015-08-01'), points: 50},
         {team: 'Chelsea', date: new Date('2015-08-01'), points: 30},
-        {team: 'Aston Villa', date: new Date('2015-08-01'), points: 20},
     ]
 });
