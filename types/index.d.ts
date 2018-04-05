2097c0c3162680b5e28066598088983ea5323d4c
Tyrone Dougherty <tyrone@tyronedougherty.com>
2018-04-05 21:54:14 +1000

    Fix default TypeScript export definition to conform to version 2.6.0 rules (#520)
    * Update package-lock.json version number to the package.json version number
    
    * Fix TypeScript definitions to conform to the tslint.json rules
    
    * Fix default type export to conform to TypeScript 2.6 rules regarding forbidden arbitrary expressions in export assignments in ambient contexts

--- types/index.d.ts
+++ types/index.d.ts
@@ -127,7 +127,7 @@ interface Emitter {
 
 interface PlotSize {
     width?: number;
-    height?: number
+    height?: number;
 }
 
 interface PlotLayout {
@@ -192,7 +192,9 @@ declare var api: {
     };
     tickPeriod: {
         get(name: string, settings?: { utc?: boolean }): { cast: (d: Date) => Date; next: (d: Date) => Date; };
-        add(name: string, period: { cast: (d: Date) => Date; next: (d: Date) => Date; }, settings?: { utc?: boolean }): void;
+        add(name: string,
+            period: { cast: (d: Date) => Date; next: (d: Date) => Date; },
+            settings?: { utc?: boolean }): void;
     };
     globalSettings: ChartSettings;
 
@@ -299,12 +301,14 @@ declare var api: {
 
 declare var version: string;
 
-export default {
-    Plot,
-    Chart,
-    api,
-    version,
+declare var _default: {
+    Plot: typeof Plot,
+    Chart: typeof Chart,
+    api: typeof api,
+    version: typeof version
 };
+
+export default _default;
 
 export {
     Plot,
